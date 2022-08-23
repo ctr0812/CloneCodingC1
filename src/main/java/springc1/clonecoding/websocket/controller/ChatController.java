a43856da2fed64d7568072b9cc260bc1e86ff2c7
ctr0812 <andy0812@naver.com>
2022-08-23 17:04:22 +0900

    소켓테스트

--- src/main/java/springc1/clonecoding/websocket/controller/ChatController.java
+++ src/main/java/springc1/clonecoding/websocket/controller/ChatController.java
@@ -17,9 +17,13 @@ import springc1.clonecoding.websocket.domain.ChatMessageDto;
         @MessageMapping("/chat/message")
         public void message(ChatMessageDto message) {
             System.out.println("pub/chat/실행되었습니다");
-            if (ChatMessage.MessageType.ENTER.equals(message.getType()))
+            if (ChatMessage.MessageType.ENTER.equals(message.getType())){
                 message.setMessage(message.getSender() + "님이 입장하셨습니다.");
-//            messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
-            messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
+            } else {
+                //   messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
+                message.setMessage(message.getSender() + "님이 입장하셨습니다.");
+                messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
+            }
+
         }
     }
