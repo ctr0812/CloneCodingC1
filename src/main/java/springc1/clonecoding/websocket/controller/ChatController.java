6cbf8566887099066e4c1b2e655e19d561d51488
ctr0812 <andy0812@naver.com>
2022-08-23 16:31:13 +0900

    수정

--- src/main/java/springc1/clonecoding/websocket/controller/ChatController.java
+++ src/main/java/springc1/clonecoding/websocket/controller/ChatController.java
@@ -5,17 +5,18 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
 import springc1.clonecoding.websocket.domain.ChatMessage;
+import springc1.clonecoding.websocket.domain.ChatMessageDto;
 
 
-
-    @RequiredArgsConstructor
+@RequiredArgsConstructor
     @Controller
     public class ChatController {
 
         private final SimpMessageSendingOperations messagingTemplate;
 
         @MessageMapping("/chat/message")
-        public void message(ChatMessage message) {
+        public void message(ChatMessageDto message) {
+            System.out.println("pub/chat/실행되었습니다");
             if (ChatMessage.MessageType.ENTER.equals(message.getType()))
                 message.setMessage(message.getSender() + "님이 입장하셨습니다.");
             messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
---
+++ src/main/java/springc1/clonecoding/websocket/domain/ChatMessageDto.java
@@ -0,0 +1,27 @@
+package springc1.clonecoding.websocket.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatMessageDto {
+
+    // 메시지 타입 : 입장, 채팅
+    public enum MessageType {
+        ENTER, TALK
+    }
+
+    private ChatMessage.MessageType type; // 메시지 타입
+
+    private String sender; // 메시지 보낸사람
+
+    private String roomId; // 방번호
+
+    private String message; // 메시지
+}
