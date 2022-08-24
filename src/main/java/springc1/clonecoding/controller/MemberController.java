edfde1e3776bd37142485609254cea5e05f62e4e
ctr0812 <andy0812@naver.com>
2022-08-24 18:26:24 +0900

    멤버 리팩토링 진행

--- src/main/java/springc1/clonecoding/controller/MemberController.java
+++ src/main/java/springc1/clonecoding/controller/MemberController.java
@@ -3,6 +3,7 @@ package springc1.clonecoding.controller;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import springc1.clonecoding.controller.request.LoginRequestDto;
 import springc1.clonecoding.controller.request.NickCheckRequestDto;
@@ -21,27 +22,26 @@ public class MemberController {
 
     //api 회원가입
     @PostMapping(value = "/user/signup")
-    public ResponseDto<?> signup(@RequestBody SignupRequestDto requestDto) {
-        return memberService.createMember(requestDto);
+    public ResponseEntity<String> signup(@RequestBody SignupRequestDto requestDto) {
+        return memberService.signup(requestDto);
     }
 
     //api 아이디 중복체크
     @PostMapping(value = "/user/signup/usercheck")
-    public ResponseDto<?> usercheck(@RequestBody UserCheckRequestDto requestDto){
+    public ResponseEntity<String> usercheck(@RequestBody UserCheckRequestDto requestDto){
         return memberService.userCheck(requestDto);
     }
 
     //api 닉네임 중복체크
     @PostMapping(value = "/user/signup/nickcheck")
-    public ResponseDto<?> usercheck(@RequestBody NickCheckRequestDto requestDto){
+    public ResponseEntity<String> nickcheck(@RequestBody NickCheckRequestDto requestDto){
         return memberService.nickCheck(requestDto);
     }
 
     //api 로그인
     @PostMapping(value = "/user/login")
-    public ResponseDto<?> login(@RequestBody LoginRequestDto requestDto,
-                                HttpServletResponse response
-    ) {
+    public ResponseEntity<String> login(@RequestBody LoginRequestDto requestDto,
+                                HttpServletResponse response) {
         return memberService.login(requestDto, response);
     }
 }
