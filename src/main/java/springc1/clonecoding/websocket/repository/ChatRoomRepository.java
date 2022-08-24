305303f65d1de9afbb7b722e48a149d00581ca3b
ctr0812 <andy0812@naver.com>
2022-08-24 15:38:29 +0900

    수정

--- src/main/java/springc1/clonecoding/websocket/repository/ChatRoomRepository.java
+++ src/main/java/springc1/clonecoding/websocket/repository/ChatRoomRepository.java
@@ -6,8 +6,11 @@ import springc1.clonecoding.domain.Product;
 import springc1.clonecoding.websocket.domain.ChatRoom;
 
 import java.util.List;
+import java.util.Optional;
 
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
     List<ChatRoom> findAllByProduct(Product findProduct);
+
+    Optional<ChatRoom> findByRoomId(String roomId);
 }
--- src/main/java/springc1/clonecoding/websocket/service/ChatService.java
+++ src/main/java/springc1/clonecoding/websocket/service/ChatService.java
@@ -3,9 +3,12 @@ package springc1.clonecoding.websocket.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import springc1.clonecoding.websocket.domain.ChatMessage;
+import springc1.clonecoding.websocket.domain.ChatRoom;
 import springc1.clonecoding.websocket.dto.request.ChatMessageDto;
 import springc1.clonecoding.websocket.repository.ChatMessageRepository;
+import springc1.clonecoding.websocket.repository.ChatRoomRepository;
 
 @RequiredArgsConstructor
 @Service
@@ -13,10 +16,15 @@ public class ChatService {
 
     private final SimpMessageSendingOperations messagingTemplate;
     private final ChatMessageRepository chatMessageRepository;
+    private final ChatRoomRepository chatRoomRepository;
 
+    @Transactional
     public void sendMessage(ChatMessageDto message) {
 
         ChatMessage chatMessage = new ChatMessage(message);
+        ChatRoom chatRoom = chatRoomRepository.findByRoomId(message.getRoomId()).orElseThrow(()-> new IllegalArgumentException("잘못된 채팅방입니다."));
+        chatMessage.setChatRoom(chatRoom);
+
         chatMessageRepository.save(chatMessage);
 
         messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
